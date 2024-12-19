f164abb5657ff3d2b101f7d9c0e012d8b978ce49
MauricioGoulartOliveira <snarfalternativa@gmail.com>
2024-12-19 12:29:14 -0300

    iniciando o projeto

---
+++ src/main/java/com/Systoque_back_end/SystoqueBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.Systoque_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystoqueBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystoqueBackEndApplication.class, args);
+	}
+
+}
